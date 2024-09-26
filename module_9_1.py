6f8425153dc64454b9e065f6ab0a056e70cb9191
tagir2211 <tagir2211@gmail.com>
2024-09-26 10:52:09 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,10 @@
+def apply_all_func(int_list, *functions):
+    result = {}
+    for func in functions:
+        result[str(func.__name__)] = func(int_list)
+    return result
+
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
